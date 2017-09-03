92c982bfc805cfc3527de60f033c8bd07146beb6
udhayakumar-p <udhayakumarp505@gmail.com>
2017-09-04 00:52:07 +0530

    Update camel.java

--- camel.java
+++ camel.java
@@ -1,12 +1,34 @@
 import java.io.*;
 import java.util.*;
-public class camel
+public class camels
 {
     public static void main(String []args)
     {
         Scanner s=new Scanner(System.in);
-      String st=s.nextLine().toUpperCase();       
+      String st=s.nextLine();
+      String str="";
+      char c=st.charAt(0);
+      str=str+Character.toUpperCase(c);
+      for(int i=1;i<st.length();i++)
+      {
+          char a=st.charAt(i);
+          char b=st.charAt(i-1);
+          if(b==' ')
+          {
+              str=str+Character.toUpperCase(a);
+          }
+          else
+          {
+              str=str+a;
+          }
+      }
+     
+           
       
-       System.out.print(st);
+       System.out.print(str);
     }
 }
+
+
+
+
