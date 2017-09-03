d753dc6635088a922db8bbc9523e94dee0575e23
udhayakumar-p <udhayakumarp505@gmail.com>
2017-09-04 01:02:52 +0530

    Create vowelsremove.java

---
+++ vowelsremove.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class vowelsremove
+{
+    public static void main(String []args)
+    {
+        Scanner s=new Scanner(System.in);
+      String st=s.nextLine();
+      String str="[aeiou]";
+      String str2=st.replaceAll(str,"");
+       System.out.print(str2);
+    }
+}
