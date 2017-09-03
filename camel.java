cb662692e6a8140b160dcdd23d52c11b540b9179
udhayakumar-p <udhayakumarp505@gmail.com>
2017-09-04 00:40:19 +0530

    Create camel.java

---
+++ camel.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class camel
+{
+    public static void main(String []args)
+    {
+        Scanner s=new Scanner(System.in);
+      String st=s.nextLine().toUpperCase();       
+      
+       System.out.print(st);
+    }
+}
